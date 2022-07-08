f6db6bff4a12008c4b0c15cdd08e5062d9a0573c
nsp111 <nsparfait1@gmail.com>
2022-07-08 10:50:12 +0200

    commit

---
+++ django_Blog/blog-web-application-django/blog/models.py
@@ -0,0 +1,12 @@
+from tkinter import CASCADE
+from django.db import models
+import uuid
+# Create your models here.
+class Blogs(models.Model):
+    
+    Image=models.ImageField(null=True,blank=True)
+    title=models.CharField(max_length=200,null=False, blank=False)
+    description=models.TextField(null=True)
+    date=models.DateField(auto_now_add=True,null=True)
+    likes=models.IntegerField(default=0)
+
